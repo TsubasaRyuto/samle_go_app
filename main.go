a31ca5273e092cbe95f47473b6ec61edf7636f2f
TsubasaRyuto <tsubasa7ryuto@gmail.com>
2023-02-19 08:59:05 +0900

    ログファイルに出力される様にした

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
   "net/http"
   "github.com/gin-gonic/gin"
   "log"
+  "github.com/TsubasaRyuto/go_sample_app/config"
 )
 
-
 func main() {
-  log.Println("staart server ...")
+  log.Println("start server ...")
+
+  log.Println(config.Config.Port)
 
   r := gin.Default()
   r.GET("/ping", func(c *gin.Context) {
